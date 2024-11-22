d5c67a19f9fe653a00218c443257ae1e295f4284
Muhammadali <muhammadalikadyrov06@gmail.com>
2024-11-22 17:32:44 +0500

    yangilash

--- posts/views.py
+++ posts/views.py
@@ -3,14 +3,25 @@ from django.shortcuts import  HttpResponse
 
 def task_create(request):
     return HttpResponse("""
-        <html>
-            <body>
-                <h1>Create Task</h1>
-                <form method="post">
-                    <input type="text" name="task_name" placeholder="Task name">
-                    <button type="submit">Create</button>
-                </form>
-            </body>
-        </html>
+       <!DOCTYPE html>
+<html lang="uz">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yangi vazifa yaratish</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>Yangi vazifa yaratish</h1>
+        <form method="post">
+            {% csrf_token %}
+            {{ form.as_p }}
+            <button type="submit" class="btn btn-primary">Vazifani saqlash</button>
+        </form>
+    </div>
+</body>
+</html>
+
     """)
 
